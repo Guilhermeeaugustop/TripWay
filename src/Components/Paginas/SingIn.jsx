060b2f01cd2c7a4d3e7d5798b2bae2a1c82a1b04
Guilhermeeaugustop <guilhermeeaugusto.p@gmail.com>
2025-10-05 11:39:34 -0400

    commit

--- src/Components/Paginas/SingIn.jsx
+++ src/Components/Paginas/SingIn.jsx
@@ -5,27 +5,30 @@ import "./SingIn.css"
 
 const SingIn = () => {
   return (
-       <div className='container1'>
-        <form>
-            <h1>Faça seu Cadastro</h1>
-            <div className='input-field'>
-                <input type="email" placeholder='Email'/>
-                <FaUser className='icon' />
-            </div>
-            <div className='input-field'>
-                <input type="password" placeholder='Senha'/>
-                <FaLock className='icon' />
+       <div>
+        <div className="Titulo2">
+          <h1>Faça seu Cadastro</h1>
+        </div>
+         <div className='container1'>
+          <form>
+              <div className='input-field'>
+                  <input type="email" placeholder='Email'/>
+                  <FaUser className='icon' />
+              </div>
+              <div className='input-field'>
+                  <input type="password" placeholder='Senha'/>
+                  <FaLock className='icon' />
+               </div>
+               <div className='input-field'>
+                  <input type="password" placeholder='Confirme sua senha'/>
+                  <FaLock className='icon' />
+               </div>
+               <Link to="/">
+                 <div className="Cadastrar"><b>Cadastrar</b></div>
+               </Link>
+          </form>
              </div>
-             <div className='input-field'>
-                <input type="password" placeholder='Confirme sua senha'/>
-                <FaLock className='icon' />
-             </div>
-             <Link to="/">
-               <div className="Cadastrar"><b>Cadastrar</b></div>
-             </Link>
-
-        </form>
-    </div>
+       </div>
   )
 }
 
