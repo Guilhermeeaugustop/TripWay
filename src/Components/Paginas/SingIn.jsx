cf81971761adc42de3417024a2a8f0075018c87b
Guilhermeeaugustop <guilhermeeaugusto.p@gmail.com>
2025-10-05 12:06:59 -0400

    commit

--- src/Components/Paginas/SingIn.jsx
+++ src/Components/Paginas/SingIn.jsx
@@ -1,6 +1,7 @@
 
 import {FaUser, FaLock} from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { MdAlternateEmail } from "react-icons/md";
 import "./SingIn.css"
 
 const SingIn = () => {
@@ -11,17 +12,20 @@ const SingIn = () => {
         </div>
          <div className='container1'>
           <form>
-              <div className='input-field'>
-                  <input type="email" placeholder='Email'/>
+              <div className='SingNome'>
+                <label htmlFor="nome"><b>Nome</b></label>
+                  <input type="nome" placeholder='Nome' className="SingNome2"/>
                   <FaUser className='icon' />
               </div>
-              <div className='input-field'>
-                  <input type="password" placeholder='Senha'/>
+              <div className='SingEmail'>
+                <label htmlFor="Email"><b>Email</b></label>
+                  <input type="email" placeholder='Email' className="SingEmail2"/>
                   <FaLock className='icon' />
-               </div>
-               <div className='input-field'>
-                  <input type="password" placeholder='Confirme sua senha'/>
-                  <FaLock className='icon' />
+              </div>
+               <div className='SingSenha'>
+                <label htmlFor="nome"><b>Senha</b></label>
+                  <input type="password" placeholder='Confirme sua senha' className="SingSenha2"/>
+                  <MdAlternateEmail className='icon' />
                </div>
                <Link to="/">
                  <div className="Cadastrar"><b>Cadastrar</b></div>
